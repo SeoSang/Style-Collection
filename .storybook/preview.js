48ba16766d8b21a65297df35a7cb31ea1c395a69
SeoSang <ddrrpg@naver.com>
2021-06-11 02:59:18 +0900

    배경화면 변경값 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,36 @@
-import "../src/index.css"
+import "../src/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: "ivory",
+    values: [
+      {
+        name: "ivory",
+        value: "#FEFFEF",
+      },
+      {
+        name: "white",
+        value: "white",
+      },
+      {
+        name: "black",
+        value: "black",
+      },
+      {
+        name: "twitter",
+        value: "#00aced",
+      },
+      {
+        name: "facebook",
+        value: "#3b5998",
+      },
+    ],
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
