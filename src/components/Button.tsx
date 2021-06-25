ea4d77326d9f9ad5e4418bb30a5f182706c42f29
SeoSang <ddrrpg@naver.com>
2021-06-25 18:01:54 +0900

    Theme 분리

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,23 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, css } from "@emotion/core";
-import { Color, grey, indigo, pink, purple, red } from "../styles/colors";
+import { jsx, css } from '@emotion/core'
+import { Color, grey, indigo, pink, purple, red } from '../styles/colors'
+import { themeColor } from '../styles/theme'
 
 type ButtonProps = {
   /** 버튼 안의 내용 */
-  children: React.ReactNode;
+  children: React.ReactNode
   /** 클릭했을 때 호출할 함수 */
-  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void
   /** 버튼의 생김새를 설정합니다. */
-  theme: "primary" | "secondary" | "error" | "pink" | "purple";
+  theme: 'primary' | 'secondary' | 'error' | 'pink' | 'purple'
   /** 버튼의 크기를 설정합니다. */
-  size: "small" | "medium" | "big";
+  size: 'small' | 'medium' | 'big'
   /** 버튼을 비활성화 시킵니다. */
-  disabled?: boolean;
+  disabled?: boolean
   /** 버튼의 너비를 임의로 설정합니다. */
-  width?: string | number;
-};
+  width?: string | number
+}
 
 /** 여러 테마를 가지고 있는 `Button` */
 const Button = ({
@@ -31,20 +32,19 @@ const Button = ({
     <button
       css={[style, themes[theme], sizes[size], { width }]}
       disabled={disabled}
-      onClick={onClick}
-    >
+      onClick={onClick}>
       {children}
     </button>
-  );
-};
+  )
+}
 
 Button.defaultProps = {
-  theme: "primary",
-  size: "medium",
-};
+  theme: 'primary',
+  size: 'medium',
+}
 
-const ANIMATION_DURATION = 0.2;
-const ANIMATION_EASING = "ease-in-out";
+const ANIMATION_DURATION = 0.2
+const ANIMATION_EASING = 'ease-in-out'
 
 const style = css`
   outline: none;
@@ -73,9 +73,9 @@ const style = css`
   &:disabled {
     cursor: not-allowed;
   }
-`;
+`
 
-const getButtonCSS = (colorObject: Color, textColor = "white") => {
+const getButtonCSS = (colorObject: Color, textColor = 'white') => {
   return css`
     background: ${colorObject[500]};
     color: ${textColor};
@@ -89,16 +89,16 @@ const getButtonCSS = (colorObject: Color, textColor = "white") => {
     &:disabled {
       color: ${colorObject[200]};
     }
-  `;
-};
+  `
+}
 
 const themes = {
-  primary: getButtonCSS(indigo),
-  secondary: getButtonCSS(grey, "black"),
-  error: getButtonCSS(red),
-  pink: getButtonCSS(pink),
-  purple: getButtonCSS(purple),
-};
+  primary: getButtonCSS(themeColor.primary),
+  secondary: getButtonCSS(themeColor.secondary, 'black'),
+  error: getButtonCSS(themeColor.error),
+  pink: getButtonCSS(themeColor.pink),
+  purple: getButtonCSS(themeColor.purple),
+}
 
 const sizes = {
   small: css`
@@ -116,6 +116,6 @@ const sizes = {
     font-size: 1.125rem;
     padding: 0 1.5rem;
   `,
-};
+}
 
-export default Button;
+export default Button
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { grey, indigo, pink, purple, red } from './colors'
+
+export const themeColor = {
+  primary: indigo,
+  secondary: grey,
+  error: red,
+  pink: pink,
+  purple: purple,
+}
