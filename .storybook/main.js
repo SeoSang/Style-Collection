1fd7d27541e02b4babbafd0e5728c59bdfe65d52
SeoSang <ddrrpg@naver.com>
2021-07-05 22:11:31 +0900

    [setting] emotion 관련 설정 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
+const path = require('path')
+
+const resolvePath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
   typescript: {
     check: false,
     checkOptions: {},
-    reactDocgen: "react-docgen-typescript",
+    reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) =>
@@ -10,14 +14,26 @@ module.exports = {
     },
   },
   stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-knobs",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs',
   ],
-};
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@emotion/core': resolvePath('node_modules/@emotion/react'),
+        '@emotion/styled': resolvePath('node_modules/@emotion/styled'),
+        'emotion-theming': resolvePath('node_modules/@emotion/react'),
+      },
+    },
+  }),
+}
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ["@emotion/babel-preset-css-prop"],
-  plugins: ["@emotion"],
-};
+  presets: ['@emotion/babel-preset-css-prop'],
+  plugins: ['@emotion'],
+}
